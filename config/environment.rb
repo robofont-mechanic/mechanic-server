cbb3803c337aa7aee25b1c3be6704779d01d0e6d
Jack Jennings <j@ckjennin.gs>
2013-04-12 14:15:23 -0400

    configure for heroku postgres

--- config/environment.rb
+++ config/environment.rb
@@ -1,17 +1,17 @@
 require 'uri'
+require 'active_record'
 require 'yaml'
 
-# Sets up database configuration
-db = URI.parse(ENV['DATABASE_URL'] || 'http://localhost')
-if db.scheme == 'postgres' # Heroku environment
-  # ActiveRecord::Base.establish_connection(
-  #   :adapter  => db.scheme == 'postgres' ? 'em_postgresql' : db.scheme,
-  #   :host     => db.host,
-  #   :username => db.user,
-  #   :password => db.password,
-  #   :database => db.path[1..-1],
-  #   :encoding => 'utf8'
-  # )
+if ENV['DATABASE_URL']
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+  ActiveRecord::Base.establish_connection(
+    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
 else # local environment
   environment = ENV['RACK_ENV'] || 'development'
   db = YAML.load(File.read('config/database.yml'))[environment]
