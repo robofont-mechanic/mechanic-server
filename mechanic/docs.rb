2771c5e8fd915b6bbee76315d285d5a9f82687eb
Jack Jennings <j@ckjennin.gs>
2014-04-06 09:40:18 -0700

    add development credit

--- mechanic/docs.rb
+++ mechanic/docs.rb
@@ -5,12 +5,12 @@ module MechanicServer
   class Docs < Sinatra::Base
     
     get '/' do
-      @extensions = Extension.order('created_at DESC').all
+      @extensions = Extension.order('created_at DESC')
       erb :index
     end
     
     get '/rss' do
-      @extensions = Extension.order('created_at DESC').limit(30).all
+      @extensions = Extension.order('created_at DESC').limit(30)
       builder :rss
     end
   end
