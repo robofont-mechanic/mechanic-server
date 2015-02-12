bb90809faa73c5b1fef6d2785e26a62b2a1dba7a
Jack Jennings <j@ckjennin.gs>
2015-02-12 15:59:51 -0800

    update styling

--- models/extension.rb
+++ models/extension.rb
@@ -1,13 +1,13 @@
 class Extension < ActiveRecord::Base
-  
+
   validates :repository, presence: true
   validates :filename, presence: true, uniqueness: { scope: :repository, message: "has already been added for this repository." }
   validates :name, presence: true, uniqueness: true
   validate :repository_exists
-  
+
   before_create :get_description
   before_create :get_author
-  
+
   def repository_exists
     return if repository.blank?
     begin
@@ -18,36 +18,42 @@ class Extension < ActiveRecord::Base
       errors.add :repository, "doesn't exist"
     end
   end
-  
-  def commits
-    @commits ||= Octokit.commits(repository)
-  end
-  
-  def files
-    Octokit.tree repository, commits.first.sha, recursive: true
-  end
-  
-  def get_description
-    self.description = Octokit.repo(repository).description
-  end
-  
-  def get_author
-    u = Octokit.user user
-    self.author = !u['name'].nil? ? u.name : u.login
-  end
-  
+
   def user
     self.repository.split('/', 2).first
   end
-  
+
   def repo
     self.repository.split('/', 2).second
   end
-  
+
+  def source
+    "http://github.com/#{repository}"
+  end
+
+  private
+
   def file_exists? filename
     files.tree.any? do |file|
       File.fnmatch "*#{filename}", file.path
     end
   end
-  
+
+  def commits
+    @commits ||= Octokit.commits(repository)
+  end
+
+  def files
+    Octokit.tree repository, commits.first.sha, recursive: true
+  end
+
+  def get_description
+    self.description = Octokit.repo(repository).description
+  end
+
+  def get_author
+    u = Octokit.user user
+    self.author = !u['name'].nil? ? u.name : u.login
+  end
+
 end
--- test/test_unit.rb
+++ test/test_unit.rb
@@ -5,30 +5,30 @@ class TestExtension < MiniTest::Unit::TestCase
     @extension = Extension.new repository: 'jackjennings/Dummy', name: 'Dummy', filename: 'Dummy.roboFontExt'
     @badExtension = Extension.new repository: 'jackjennings/Dummy2', name: 'Dummy', filename: 'Dummy.roboFontExt'
   end
-  
+
   def test_has_user
     assert_equal 'jackjennings', @extension.user
   end
-  
+
   def test_has_repo
     assert_equal 'Dummy', @extension.repo
   end
-  
+
   def test_valid_if_repository_exists
     assert @extension.valid?, @extension.errors.full_messages
   end
-  
+
   def test_invalid_if_repository_doesnt_exist
     assert !@badExtension.valid?
   end
-  
+
   def test_gets_description
-    @extension.get_description
+    @extension.send :get_description
     assert !@extension.description.blank?
   end
 
   def test_gets_author
-    @extension.get_author
+    @extension.send :get_author
     assert !@extension.author.blank?
   end
 end
