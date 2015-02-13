0c8d8b8ba107c970a33cd761adb519f616701506
Jack Jennings <j@ckjennin.gs>
2015-02-12 16:06:57 -0800

    brute force sorting

--- mechanic/docs.rb
+++ mechanic/docs.rb
@@ -4,7 +4,7 @@ require 'builder'
 module Mechanic
   class Docs < Sinatra::Base
     get '/' do
-      @extensions = Extension.order('created_at DESC')
+      @extensions = Extension.all.sort_by {|e| e.name.downcase}
       erb :index
     end
 
