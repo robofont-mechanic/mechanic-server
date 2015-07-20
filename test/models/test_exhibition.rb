1a72dacb90498d3b8f65adb7cf46295aa8fdceb5
Jack Jennings <jack@remote.gd>
2015-07-19 18:45:38 -0700

    remove redundant object creation in extension test

--- test/models/test_exhibition.rb
+++ test/models/test_exhibition.rb
@@ -5,41 +5,49 @@ require 'mechanic/server/models/extension'
 module Mechanic
   class TestExtension < Minitest::Test
 
-    def setup
-      @extension = Extension.new repository: 'jackjennings/Dummy', name: 'Dummy', filename: 'Dummy.roboFontExt'
-      @badExtension = Extension.new repository: 'jackjennings/Dummy2', name: 'Dummy', filename: 'Dummy.roboFontExt'
-    end
-
     def test_has_user
-      assert_equal 'jackjennings', @extension.user
+      assert_equal 'jackjennings', build_extension.user
     end
 
     def test_has_repo
-      assert_equal 'Dummy', @extension.repo
+      assert_equal 'Dummy', build_extension.repo
     end
 
     def test_valid_repository_name
-      @extension.repository = 'jackjennings/Dummy/foobar'
-      refute @extension.valid?
-      assert @extension.errors.full_messages.length > 0
+      extension = build_extension repository: 'jackjennings/Dummy/foobar'
+      refute extension.valid?
+      assert extension.errors.full_messages.length > 0
     end
 
     def test_valid_if_repository_exists
-      assert @extension.valid?, @extension.errors.full_messages
+      extension = build_extension
+      assert extension.valid?, extension.errors.full_messages
     end
 
     def test_invalid_if_repository_doesnt_exist
-      assert !@badExtension.valid?
+      assert !build_extension(repository: 'jackjennings/Dummy2').valid?
     end
 
     def test_gets_description
-      @extension.send :get_description
-      assert !@extension.description.blank?
+      extension = build_extension
+      extension.send :get_description
+      assert !extension.description.blank?
     end
 
     def test_gets_author
-      @extension.send :get_author
-      assert !@extension.author.blank?
+      extension = build_extension
+      extension.send :get_author
+      assert !extension.author.blank?
+    end
+
+    private
+
+    def build_extension(attrs = {})
+      Extension.new attrs.reverse_merge({
+        repository: 'jackjennings/Dummy',
+        name: 'Dummy',
+        filename: 'Dummy.roboFontExt'
+      })
     end
 
   end
